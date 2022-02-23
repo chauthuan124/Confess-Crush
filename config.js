c9d304d8d77e9f6a8c0fa95a868678455a306614
anhdeptrai <96155960+chauthuan124@users.noreply.github.com>
2022-02-23 08:52:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const CONFIG = {
     titleWeb: "fromlttwithlove",
-    introTitle: 'gửi tới mf Nhi',
+    introTitle: 'gửi tới mf Vinh',
     introDesc: `t thì F0 m thì F1 nhưng cùng điểm chung là FA`,
     btnIntro: '^^HiHi^^',
-    title: 'Every time I see you, I get lost. If I’m dreaming, please don’t wake me up🥰',
+    title: 'Nhớ ở lớp đừng nói ai chuyện kia nha🥰',
     desc: 'Cảm ơn gió, cảm ơn mưa, cảm ơn định mệnh đã cho chúng ta gặp nhau ',
     btnYes: 'Thích lắm <33',
     btnNo: 'không thik sến quá! :33',
@@ -11,7 +11,7 @@ const CONFIG = {
     btnReply: 'Gửi cho bạn <3',
     reply: 'soái ca, thông minh, ga lăng,mạnh mẽ, hài hước,hiền lành,... ',
     mess: 'Mình biết mà 🥰 có cần khen đến v đâu',
-    messDesc: 'Nhớ ún nước cam chanh nước lọc nhiều để khỏi dính gì nha :)))',
+    messDesc: 'Nhớ chép bài học bài có gì t lên chỉ t nha',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
